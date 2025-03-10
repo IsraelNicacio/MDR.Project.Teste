9de4396ec52ceadbefa7947230ade5c1ae39d177
Israel Nicacio <israel.nicacio@outlook.com>
2025-03-10 11:10:45 -0300

    Finalizado camada de servicos

---
+++ src/Cadastro/MDR.Cadastro.Application/AutoMapper/DomainDTOMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MDR.Cadastro.Application.DTO;
+using MDR.CadastroDomain.Entities;
+
+namespace MDR.Cadastro.Application.AutoMapper;
+
+public class DomainDTOMappingProfile : Profile
+{
+    public DomainDTOMappingProfile()
+    {
+        CreateMap<Pessoa, PessoaDTO>().ReverseMap();
+        CreateMap<Departamento, DepartamentoDTO>().ReverseMap();
+    }
+}
---
+++ src/Cadastro/MDR.Cadastro.Application/Services/IPessoaService.cs
@@ -0,0 +1,14 @@
+﻿using MDR.Cadastro.Application.DTO;
+
+namespace MDR.Cadastro.Application.Services;
+
+public interface IPessoaService
+{
+    Task<IEnumerable<PessoaDTO>> RecuperarPessoas();
+    Task<PessoaDTO> RecuperarPessoaPorId(Guid id);
+    Task<IEnumerable<PessoaDTO>> RecuperarPessoaPorDepartamento(Guid id);
+    void AdicionarPessoa(PessoaDTO pessoa);
+    void EditarPessoa(PessoaDTO pessoa);
+
+    
+}
