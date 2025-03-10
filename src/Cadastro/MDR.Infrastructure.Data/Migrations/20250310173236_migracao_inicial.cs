39d282b3594338f84be01b92ac02dcba64347192
Israel Nicacio <israel.nicacio@outlook.com>
2025-03-10 14:58:41 -0300

    Adicionado migracao

---
+++ src/Cadastro/MDR.Infrastructure.Data/Migrations/20250310173236_migracao_inicial.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MDR.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class migracao_inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departamento",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(120)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamento", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DepartamentoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(60)", nullable: false),
+                    Sobrenome = table.Column<string>(type: "varchar(60)", nullable: false),
+                    Idade = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pessoas_Departamento_DepartamentoId",
+                        column: x => x.DepartamentoId,
+                        principalTable: "Departamento",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pessoas_DepartamentoId",
+                table: "Pessoas",
+                column: "DepartamentoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+
+            migrationBuilder.DropTable(
+                name: "Departamento");
+        }
+    }
+}
---
+++ src/Cadastro/SQL/MigracaoInicial.sql
@@ -0,0 +1,35 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Departamento] (
+    [Id] uniqueidentifier NOT NULL,
+    [Nome] varchar(120) NOT NULL,
+    CONSTRAINT [PK_Departamento] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Pessoas] (
+    [Id] uniqueidentifier NOT NULL,
+    [DepartamentoId] uniqueidentifier NOT NULL,
+    [Nome] varchar(60) NOT NULL,
+    [Sobrenome] varchar(60) NOT NULL,
+    [Idade] integer NOT NULL,
+    CONSTRAINT [PK_Pessoas] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Pessoas_Departamento_DepartamentoId] FOREIGN KEY ([DepartamentoId]) REFERENCES [Departamento] ([Id]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_Pessoas_DepartamentoId] ON [Pessoas] ([DepartamentoId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250310173236_migracao_inicial', N'9.0.2');
+
+COMMIT;
+GO
+
