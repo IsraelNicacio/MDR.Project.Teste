6eebec6ebdc60f41c42371bb58f189c17b0c00c0
Israel Nicacio <israel.nicacio@outlook.com>
2025-03-10 12:05:03 -0300

    Extracao da classe de configuracao do tenant

--- src/Cadastro/MDR.Cadastro.Api/Extensions/DepenciesInjections.cs
+++ src/Cadastro/MDR.Cadastro.Api/Extensions/DepenciesInjections.cs
@@ -2,10 +2,8 @@
 using MDR.Cadastro.Application.AutoMapper;
 using MDR.Cadastro.Application.Services;
 using MDR.CadastroDomain.Repositories;
-using MDR.Infrastructure.Data.Context;
 using MDR.Infrastructure.Data.Repositories;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
 
 namespace MDR.Cadastro.Api.Extensions;
@@ -19,27 +17,5 @@ public static class DepenciesInjections
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<IPessoaService, PessoaService>();
         services.AddScoped<IDepartamentoService, DepartamentoService>();
-    }
-}
-
-public static class TenantConfiguration
-{
-    public static void RegisterTenant(this  IServiceCollection services, ConfigurationManager configuration)
-    {
-        services.AddHttpContextAccessor();
-
-        services.AddScoped<CadastroContext>(provider =>
-        {
-            var optionBuilder = new DbContextOptionsBuilder<CadastroContext>();
-            var httpContext = provider.GetService<IHttpContextAccessor>()?.HttpContext;
-            var tenant = httpContext.Request.Path.Value?.Split("/", StringSplitOptions.RemoveEmptyEntries)[0];
-            var connectionString = configuration.GetConnectionString("TenantDataBase")?.Replace("_DATABASE_", tenant);
-
-            optionBuilder.UseSqlServer(connectionString)
-            .LogTo(Console.WriteLine)
-            .EnableSensitiveDataLogging();
-
-            return new CadastroContext(optionBuilder.Options);
-        });
     }
 }
